538aed3cad235c5376875bde6d0a2543d7faa780
Lucas Walbeck <lucas.walbeck@flyersenergy.com>
2019-08-14 16:09:56 -0700

    added initial blog component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Recipes from './components/Recipes';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Blog from './components/Blog';
 import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -18,7 +18,7 @@ function App() {
       <AboutMe></AboutMe>
       <Skills></Skills>
       <Projects apiCall="/projects/"/>
-      <Recipes apiCall="/recipes/"/>
+      <Blog apiCall="/posts/"/>
     </div>
   );
 }
---
+++ src/components/Blog.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import API from '../utils/API';
+import Fade from 'react-reveal/Fade'
+import { Card, Button } from 'react-bootstrap';
+
+class Blog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            loaded: false,
+            apiCall: props.apiCall
+        };
+    }
+    render() {
+        const { posts, loaded } = this.state;
+        return(
+            <Fade when={loaded}>
+                <div>
+                    <h2 style={{margin: '25px'}}>Blog</h2>
+                    <div>{posts}</div>
+                </div>
+            </Fade>
+        );
+    }
+
+    async componentDidMount() {
+        let data = await API.get(this.state.apiCall);
+        let arr = [];
+        data = data.data;
+        for (var i = 0; i < data.length; i++) {
+            arr.push(<Post item={data[i]} key={i}></Post>);
+        }
+        this.setState({ posts: arr, loaded: true }); 
+    }
+}
+export default Blog;
+
+class Post extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: props.item
+        }
+    }
+
+    render() {
+        const { post } = this.state;
+        return(
+            <Card style={{ maxWidth: '25rem', margin:'0 25px 10px 25px' }}>
+                <Card.Img variant="top" src={post.image} />
+                <Card.Body>
+                    <Card.Title>{post.title} - {post.updated_at.split("T")[0]}</Card.Title>
+                    <Card.Text style={{ overflow: 'auto', height: '10em'}}>
+                        {post.content}
+                    </Card.Text>
+                    <Button variant="primary">
+                        More Information
+                    </Button>
+
+                </Card.Body>
+            </Card>
+        );
+    }
+}
