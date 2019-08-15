8c1c078a4ba9183d473ba16de7407183872cb6bd
Lucas Walbeck <lucas.walbeck@flyersenergy.com>
2019-08-15 11:19:00 -0700

    Updates

---
+++ precache-manifest.339ae0403190baba2ed140bfaf3c4b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f7237569d6f8dd6f6c5c5ebc25c09d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e14d8179e5fb998c0e6f",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "a9cb5141a2c2798ef2bb",
+    "url": "/static/js/2.2a4eaf0c.chunk.js"
+  },
+  {
+    "revision": "e14d8179e5fb998c0e6f",
+    "url": "/static/js/main.1b5d6a29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b984234241f707a21d82af04ff362c3b",
+    "url": "/static/media/IMG_0376.b9842342.jpg"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://lwalbeck.pythonanywhere.com/",
-    responseType: "json"
-});
